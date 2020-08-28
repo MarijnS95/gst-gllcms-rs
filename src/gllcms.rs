b0f15bd2bc387020ad3cfad7758bf113582be050
Marijn Suijten <marijns95@gmail.com>
2020-08-28 10:37:44 +0200

    Add pads configured for GL input/output

--- src/gllcms.rs
+++ src/gllcms.rs
@@ -2,17 +2,17 @@ use gst::glib;
 use gst::prelude::*;
 use gst::subclass::prelude::*;
 use gst_base::subclass::prelude::*;
+use gst_gl::*;
 use gstreamer as gst;
 use gstreamer_base as gst_base;
+use gstreamer_gl as gst_gl;
 use gstreamer_video as gst_video;
 
 use glib::subclass;
 use glib::subclass::prelude::*;
 
-// use std::i32;
-use std::sync::Mutex;
-
 use once_cell::sync::Lazy;
+use std::sync::Mutex;
 
 struct GlLcms {}
 
@@ -52,6 +52,47 @@ impl ObjectSubclass for GlLcms {
             false,
             false,
         );
+
+        let caps = gst::Caps::builder("video/x-raw")
+            .features(&[
+                &CAPS_FEATURE_MEMORY_GL_MEMORY,
+                // TODO: meta:GstVideoOverlayComposition? That'd be only for input though!
+                // &gst_video::CAPS_FEATURE_META_GST_VIDEO_OVERLAY_COMPOSITION,
+            ])
+            .field("format", &gst_video::VideoFormat::Rgba.to_string())
+            .field("texture-target", &gst::List::new(&[&"2D", &"external-oes"]))
+            // .field("width", &gst::IntRange::<i32>::new(0, i32::MAX))
+            // .field("height", &gst::IntRange::<i32>::new(0, i32::MAX))
+            // .field(
+            //     "framerate",
+            //     &gst::FractionRange::new(gst::Fraction::new(0, 1), gst::Fraction::new(i32::MAX, 1)),
+            // )
+            // TODO: framerate/width/height fields are optional?
+            .build();
+
+        gst::gst_debug!(CAT, "Using caps {} for input and output", caps);
+
+        let src_pad_template = gst::PadTemplate::new(
+            "src",
+            gst::PadDirection::Src,
+            gst::PadPresence::Always,
+            &caps,
+        )
+        .unwrap();
+
+        gst::gst_debug!(CAT, "Src pad template {:?}", &src_pad_template,);
+        klass.add_pad_template(src_pad_template);
+
+        let sink_pad_template = gst::PadTemplate::new(
+            "sink",
+            gst::PadDirection::Sink,
+            gst::PadPresence::Always,
+            &caps,
+        )
+        .unwrap();
+
+        gst::gst_debug!(CAT, "Sink pad template {:?}", &sink_pad_template);
+        klass.add_pad_template(sink_pad_template);
     }
 }
 
